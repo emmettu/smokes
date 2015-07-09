a36247356cc354156fd7fa4394935b62d23680b3
Emmett Underhill <eunderhi@redhat.com>
2015-07-09 16:48:35 -0400

    Adding error message output to smoke script
    Smoke script now prints the grep of the standalone error logs

--- smoke.sh
+++ smoke.sh
@@ -18,3 +18,10 @@ for i in `ps -aux | grep Standalone | gawk '{ print $2 }'`; do kill -9 $i; done
 for i in `ps -aux | grep standalone | gawk '{ print $2 }'`; do kill -9 $i; done
 for i in `ps -aux | grep Domain | gawk '{ print $2 }'`; do kill -9 $i; done
 for i in `ps -aux | grep domain | gawk '{ print $2 }'`; do kill -9 $i; done
+
+cd ~/jboss-eap-6.4/standalone/log
+figlet !!!ERRORS!!!
+echo "Hey, nice job if you're reading this then there aren't any errors!"
+echo "However if there's stuff after this message then there ARE errors"
+echo "in which case I revoke the nice job."
+grep -RHEin 'ERROR|FATAL|EXCEPT' . | grep -v DEBUG
