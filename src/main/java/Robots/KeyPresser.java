04ae6f8905b2daf4aa18f107be736f696fa61a3c
Emmett Underhill <eunderhi@redhat.com>
2015-07-09 15:34:18 -0400

    Adjusted username password input method
    Instead of manually hardcoding every key event required to type the
    username and password into the authentication dialogue, the clipboard
    is set to the username/password and the robot presses control v to paste
    it in instead. This is a much better method.

--- src/main/java/Robots/KeyPresser.java
+++ src/main/java/Robots/KeyPresser.java
@@ -2,6 +2,7 @@ package Robots;
 
 import java.awt.*;
 import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.awt.event.KeyEvent;
 
 /**
@@ -15,57 +16,40 @@ import java.awt.event.KeyEvent;
 public class KeyPresser {
 
     private Robot robot;
-    private final static String USERNAME = "admin";
-    private final static String PASSWORD = "qwer#1234";
-
-    public void enterUsernameAndPassword() {
-        robot.keyPress(KeyEvent.VK_A);
-        robot.keyRelease(KeyEvent.VK_A);
-        robot.keyPress(KeyEvent.VK_D);
-        robot.keyRelease(KeyEvent.VK_D);
-        robot.keyPress(KeyEvent.VK_M);
-        robot.keyRelease(KeyEvent.VK_M);
-        robot.keyPress(KeyEvent.VK_I);
-        robot.keyRelease(KeyEvent.VK_I);
-        robot.keyPress(KeyEvent.VK_N);
-        robot.keyRelease(KeyEvent.VK_N);
-        robot.keyPress(KeyEvent.VK_TAB);
-        robot.keyRelease(KeyEvent.VK_TAB);
-        robot.keyPress(KeyEvent.VK_Q);
-        robot.keyRelease(KeyEvent.VK_Q);
-        robot.keyPress(KeyEvent.VK_W);
-        robot.keyRelease(KeyEvent.VK_W);
-        robot.keyPress(KeyEvent.VK_E);
-        robot.keyRelease(KeyEvent.VK_E);
-        robot.keyPress(KeyEvent.VK_R);
-        robot.keyRelease(KeyEvent.VK_R);
-        robot.keyPress(KeyEvent.VK_SHIFT);
-        robot.keyPress(KeyEvent.VK_3);
-        robot.keyRelease(KeyEvent.VK_3);
-        robot.keyRelease(KeyEvent.VK_SHIFT);
-        robot.keyPress(KeyEvent.VK_1);
-        robot.keyRelease(KeyEvent.VK_1);
-        robot.keyPress(KeyEvent.VK_2);
-        robot.keyRelease(KeyEvent.VK_2);
-        robot.keyPress(KeyEvent.VK_3);
-        robot.keyRelease(KeyEvent.VK_3);
-        robot.keyPress(KeyEvent.VK_4);
-        robot.keyRelease(KeyEvent.VK_4);
-        robot.keyPress(KeyEvent.VK_ENTER);
-        robot.keyRelease(KeyEvent.VK_ENTER);
-    }
-    private void pastePassword() {
-
-    }
+    private final static StringSelection USERNAME = new StringSelection("admin");
+    private final static StringSelection PASSWORD = new StringSelection("qwer#1234");
 
     public KeyPresser() {
         makeRobot();
     }
-
     private void makeRobot() {
         try {
             robot = new Robot();
         } catch (AWTException e) {
+            e.printStackTrace();
+        }
+    }
+    public void enterUsernameAndPassword() {
+        pasteWord(USERNAME);
+        robot.keyPress(KeyEvent.VK_TAB);
+        robot.keyRelease(KeyEvent.VK_TAB);
+        pasteWord(PASSWORD);
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_ENTER);
+    }
+    private void pasteWord(StringSelection word) {
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(word, null);
+        robot.keyPress(KeyEvent.VK_CONTROL);
+        robot.keyPress(KeyEvent.VK_V);
+        robot.keyRelease(KeyEvent.VK_V);
+        robot.keyRelease(KeyEvent.VK_CONTROL);
+        waitForPaste();
+    }
+    private void waitForPaste() {
+        try {
+            Thread.sleep(20);
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
